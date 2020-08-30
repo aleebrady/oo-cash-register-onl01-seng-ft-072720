d77c980f4ef4030c4da3b396e5df14fb811c1367
Aaron Brady <aleebrady@gmail.com>
2020-08-30 06:54:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,6 +14,13 @@ class CashRegister
     @total += item[:price] * item[:quantity]
     item
   end
+  
+  
+  def apply_discount
+    if @discount > 0 
+      binding.pry
+      discount = total * (@discount.to_f / 100)
+  end
  
  
 end
